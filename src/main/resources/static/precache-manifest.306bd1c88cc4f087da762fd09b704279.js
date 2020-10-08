0c7e32778fa4a2827370679de9f39820debdfc9f
TomaSam <toma.samulionyte@gmail.com>
2020-10-08 19:39:40 +0300

    Add frontend

---
+++ src/main/resources/static/precache-manifest.306bd1c88cc4f087da762fd09b704279.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0efc894e5f96f77d9cc8523029ac8ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7fe8a3c9a25e0f65dc1",
+    "url": "/static/css/main.0a805d99.chunk.css"
+  },
+  {
+    "revision": "6c83f4d0eee9db162dc3",
+    "url": "/static/js/2.d02d5c1a.chunk.js"
+  },
+  {
+    "revision": "f1882e7d89cef6386fde43da35464e77",
+    "url": "/static/js/2.d02d5c1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7fe8a3c9a25e0f65dc1",
+    "url": "/static/js/main.cdec94a4.chunk.js"
+  },
+  {
+    "revision": "36533a3746c3b0e6a511",
+    "url": "/static/js/runtime-main.3a2ce01e.js"
+  }
+]);
